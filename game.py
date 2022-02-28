d9c32f6f017c55cb2ca731c1dbe5f3ac83284a44
payown <michael@yourownpay.com>
2022-02-27 20:52:48 -0800

    Personalizing messages in the game, updated readme, and allowed user to pick how mini guesses they want

--- game.py
+++ game.py
@@ -1,13 +1,20 @@
 import random
+import sys
 correctNumber = random.randint(1,100)
-userNumber = int(input("Enter your number between 1 and 100"))
-if userNumber == correctNumber:
-    print ("Cool, you guessed the correct number")
-elif userNumber >= correctNumber:
-    print ("Sorry, your number is to high, would you like to try again? Enter Y/N")
-elif userNumber <= correctNumber:
-    print ("Sorry, your number is to low")
-
+name = str(input("What is your name?"))
+tries = int(input(f"Hi {name}, how mini guesses would you like?"))
+attempts = 0
+while attempts <= tries:
+    guess = int(input(f"ok so {name}, what's your guess?"))
+    if guess < correctNumber:
+        print(f"Sorry {name}, your guess is to low")
+        attempts = attempts+1
+    elif guess > correctNumber:
+        print(f"Sorry {name}, your guess is to high")
+        attempts = attempts+1
+    elif guess == correctNumber:
+        print(f"Congrats {name}, you got it")
+        sys.exit()
 else:
-    print ("I don't understand that guess, please try again")
-    
+        print("Error, no number guessed.")
+        quit()
--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
-# this is a readme file
+# Readme
 
-this is my readme file for testing with github.
+Last Updated: 27 FEB, 2022 8:45 PM PT
 
+# v 0.2
+
+
+Asks for users name
+personalise messages in game
+asked user what they want to guess
+added quit abilities
+
+# V0.1
+
+used python to prompt the member to guess a number
